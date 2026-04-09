3100b3bc8d8c837d6479cc3e9493cf8e778c3081
dreuse <sueder.morais@recordedfuture.com>
2026-04-09 15:27:40 +0200

    Update cask formula with release SHA-256

--- Formula/colimabar.rb
+++ Formula/colimabar.rb
@@ -1,6 +1,6 @@
 cask "colimabar" do
   version "0.1.0"
-  sha256 "PLACEHOLDER_SHA256"
+  sha256 "86e60ffe1f251db4e8f365cdaeb6003195750ebb38ff81f99d1dd5103e4a84c7"
 
   url "https://github.com/dreuse/ColimaBar/releases/download/v#{version}/ColimaBar.zip"
   name "ColimaBar"
